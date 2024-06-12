d90e78a59bffe63e00770edb7161ab3cc9e57916
Chandan Kumar <149804185+chandankumarm55@users.noreply.github.com>
2024-06-12 23:32:42 +0530

    Update userController.js

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -76,7 +76,7 @@ export const login = async(req, res) => {
         const token = await jwt.sign(tokenData, process.env.JWT_KEY, { expiresIn: '1d' });
 
 
-        return res.status(200).cookie("token", token, { maxAge: 1 * 24 * 60 * 60 * 1000, httpOnly: true, sameSite: 'strict' }).json({
+        return res.status(200).cookie("token", token, { maxAge: 7 * 24 * 60 * 60 * 1000, httpOnly: true, sameSite: 'strict' }).json({
             message: "Login successful",
             _id: user._id,
             username: user.username,
