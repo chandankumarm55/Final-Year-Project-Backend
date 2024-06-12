a14d589b39b3b7882f5e94d52048cae53986d7d5
Chandan Kumar <149804185+chandankumarm55@users.noreply.github.com>
2024-06-12 23:40:27 +0530

    Update socket.js

--- socket/socket.js
+++ socket/socket.js
@@ -1,13 +1,14 @@
 import { Server } from "socket.io";
 import http from "http";
 import express from "express";
+import cors from 'cors'; // Import cors module
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: ['http://localhost:3000'],
-        methods: ['GET', 'POST'],
+        origin: process.env.FRONT_END_URL || 'https://final-year-project-frontend-kds2.vercel.app',
+        credentials: true
     },
 });
 
