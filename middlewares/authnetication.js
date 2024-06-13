ed813f35425fe63e8f4784777e6acbc4e2f6a916
Chandan Kumar <149804185+chandankumarm55@users.noreply.github.com>
2024-06-13 22:17:54 +0530

    Update authnetication.js

--- middlewares/authnetication.js
+++ middlewares/authnetication.js
@@ -1,25 +1,26 @@
-import jwt from 'jsonwebtoken'
+import jwt from 'jsonwebtoken';
 
-const isAuthenticated = async(req, res, next) => {
-    try {
-        const token = req.cookies.token;
-        console.log('token',token)
-        if (!token) {
-            return res.status(401).json({ message: "User Not authenticated" })
-        }
-        const decode = await jwt.verify(token, process.env.JWT_KEY)
-        console.log('decode',decode)
-
-        if (!decode) {
-            return res.status(401).json({ message: 'Invaild Token' })
-        };
-        req.id = decode.userId;
-         console.log('req.id',req.id)
-        next()
-
-    } catch (error) {
-        console.log(error)
+const isAuthenticated = async (req, res, next) => {
+  try {
+    const token = req.cookies.token;
+    console.log('token', token);
+    if (!token) {
+      return res.status(401).json({ message: "User Not authenticated" });
     }
-}
+
+    const decode = await jwt.verify(token, process.env.JWT_KEY);
+    console.log('decode', decode);
+    if (!decode) {
+      return res.status(401).json({ message: 'Invalid Token' });
+    }
+
+    req.id = decode.userId;
+    console.log('req.id', req.id);
+    next();
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: 'Internal server error' });
+  }
+};
 
 export default isAuthenticated;
